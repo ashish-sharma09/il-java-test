f78cf9ebd033068530dee0ed595bd34cb24d7889
Ashish Sharma <ashish.sharma2@tesco.com>
2022-02-14 14:00:09 +0000

    Added test for adding incorrect quantity while adding an item

--- app/src/main/java/co/uk/henry/basket/BasketServiceImpl.java
+++ app/src/main/java/co/uk/henry/basket/BasketServiceImpl.java
@@ -7,13 +7,21 @@ public class BasketServiceImpl implements BasketService {
 
     @Override
     public void add(Item item, int quantity) {
+        // pre-assertions
         if (item == null) {
-            throw new IllegalArgumentException("An item cannot be null");
+            throwIllegalArgument("An item cannot be null");
+        }
+        if (quantity <= 0) {
+            throwIllegalArgument("An item quantity cannot be less than or equal to 0");
         }
     }
 
     @Override
     public Basket getBasket() {
         return null;
+    }
+
+    private void throwIllegalArgument(String An_item_cannot_be_null) {
+        throw new IllegalArgumentException(An_item_cannot_be_null);
     }
 }
--- app/src/test/groovy/co/uk/henry/basket/BasketServiceImplSpec.groovy
+++ app/src/test/groovy/co/uk/henry/basket/BasketServiceImplSpec.groovy
@@ -1,5 +1,7 @@
 package co.uk.henry.basket
 
+import co.uk.henry.model.Item
+import co.uk.henry.model.Unit
 import spock.lang.Specification
 
 class BasketServiceImplSpec extends spock.lang.Specification {
@@ -15,5 +17,25 @@ class BasketServiceImplSpec extends spock.lang.Specification {
 
         and: "Appropriate error message exists"
         exception.message == "An item cannot be null"
+    }
+
+    @Unroll
+    def "An item cannot be added with quantity as #quantity"() {
+        given: "An item to be added to the basket"
+        Item item = new Item("someItemCode", "someItemName", Unit.SINGLE, 1.0)
+
+        when: "item with #quantity"
+        basketService.add(item, quantity)
+
+        then: "An illegal argument exception is reported"
+        def exception = thrown(IllegalArgumentException)
+
+        and: "An appropriate error message exists"
+        exception.message == "An item quantity cannot be less than or equal to 0"
+
+        where:
+        quantity | _
+        0        | _
+        -1       | _
     }
 }
