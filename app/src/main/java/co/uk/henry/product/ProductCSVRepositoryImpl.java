2dd3bad348d1c5f89e6b61d2d30143d9423b3bd1
Ashish Sharma <ashish.sharma2@tesco.com>
2022-02-14 15:01:24 +0000

    Passed test for fetching items from CSV file

--- app/src/main/java/co/uk/henry/product/ProductCSVRepositoryImpl.java
+++ app/src/main/java/co/uk/henry/product/ProductCSVRepositoryImpl.java
@@ -1,12 +1,16 @@
 package co.uk.henry.product;
 
 import co.uk.henry.model.Item;
+import co.uk.henry.model.Unit;
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProductCSVRepositoryImpl implements ProductRepository {
 
@@ -15,14 +19,29 @@ public class ProductCSVRepositoryImpl implements ProductRepository {
     public ProductCSVRepositoryImpl(final Path itemsCSVFilePath) {
         try {
             this.csvReader = new CSVReader(new FileReader(itemsCSVFilePath.toFile()));
-        } catch (FileNotFoundException e) {
-            throw new IllegalStateException(String.format("Given CSV file with path: %s does not exist.", itemsCSVFilePath));
+        } catch (FileNotFoundException fileNotFoundException) {
+            throw new IllegalStateException(
+                    String.format("Given CSV file with path: %s does not exist.", itemsCSVFilePath)
+            );
         }
     }
 
     @Override
     public List<Item> getProducts() {
+        try {
+            final List<String[]> itemRows = csvReader.readAll();
+            itemRows.remove(0);
 
+            final List<Item> parsedItems = itemRows.stream().map(
+                    row -> new Item(row[0], row[1], Unit.valueOf(row[2].toUpperCase()), Double.parseDouble(row[3]))
+            ).collect(Collectors.toList());
+
+            return parsedItems;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CsvException e) {
+            throw new ProductServiceException("Error while fetching products", e);
+        }
         return null;
     }
 }
